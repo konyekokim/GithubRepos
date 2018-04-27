022403ee451a9c8bc510a49974794924c0d61b70
Konye <konyekokim@gmail.com>
2018-04-27 09:57:30 +0100

    Initial commit

---
+++ app/src/main/java/com/chokus/konye/githubrepos/ReposListActivity.kt
@@ -0,0 +1,12 @@
+package com.chokus.konye.githubrepos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReposListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repos_list)
+    }
+}
