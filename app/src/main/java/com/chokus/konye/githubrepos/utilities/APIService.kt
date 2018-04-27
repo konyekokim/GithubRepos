bb4bfae4a6d48484681265172e7716ff134b02cf
Konye <konyekokim@gmail.com>
2018-04-27 21:25:29 +0100

    remove unnecessary  comment

--- app/src/main/java/com/chokus/konye/githubrepos/utilities/APIService.kt
+++ app/src/main/java/com/chokus/konye/githubrepos/utilities/APIService.kt
@@ -7,12 +7,7 @@ import retrofit2.http.Query
 
 interface APIService {
 
-    //?q=android%20language:java&sort=stars&order=desc
-    ///// This query fetches most trending android repository /////
-    ///// For demo purpose we would call it directly
-    //?page=2&per_page=100'
     //this query fetches most trending android repos... for demo purposes we are calling it directly
-
     @GET("search/repositories?q=android%20language:java&sort=stars&order=desc&per_page=10")
     fun getRepositories(@Query("page") page: Int): Call<RepoModel>
 }
